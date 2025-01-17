2c79f22458384fe125db8f86964321c7cabdfa7d
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-17 15:33:01 +0100

    Add Correction ASP .net MVC : Les Vues

---
+++ ASP-Exos-Vues/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Exos_Vues.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Register");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
